412e3fc3c79ec8dd484d01a33d9f38fe4a01cc65
Bongumusa Joseph Hlongwane <94066555+Joemusa@users.noreply.github.com>
2026-04-24 16:13:15 +0200

    Simplify growth section layout and KPIs
    Reduced the number of columns in the growth section from 7 to 4 and removed KPI displays for new visitors, absent members, and tithing members.

--- app.py
+++ app.py
@@ -425,7 +425,7 @@ with tab1:
 # GROWTH
 # ============================
 with tab2:
-    k1, k2, k3, k4, k5, k6, k7 = st.columns(7)
+    k1, k2, k3, k4, = st.columns(4)
     with k1:
         show_kpi("Total Members", members_f["MemberID"].nunique())
 
@@ -437,15 +437,7 @@ with tab2:
 
     with k4:
         show_kpi("Present Members", len(attendance_f))
-
-    with k5:
-        show_kpi("New Visitors", len(new_visitors))
-
-    with k6:
-        show_kpi("Absent Members", len(members_not_attending))
-
-    with k7:
-        show_kpi("Tithing Members", len(tithing))
+        
     st.markdown('<div class="chart-card">', unsafe_allow_html=True)
 
     mem_base = members.dropna(subset=["Timestamp"]).copy()
