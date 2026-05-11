14e85c8452c8d7e74ca0882eb8ddb7c6b466949d
Bongumusa Joseph Hlongwane <94066555+Joemusa@users.noreply.github.com>
2026-05-11 17:30:47 +0200

    Fix formatting issue in app.py

--- app.py
+++ app.py
@@ -376,7 +376,7 @@ tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9, tab10, tab11 = st.tabs([
     "💰 Tithing Members",
     "📩 Bulk SMS",
     "🗺️ Map",
-    "Events"
+    "Events",
     "Intertional Contacts"
 ])
 
